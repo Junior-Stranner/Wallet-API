41f785b548dfce77c1d23ec19701ecfb21ea4480
Junior-Stranner <JuniorStranner@gmail.com>
2025-05-07 16:15:21 -0300

    first commit

---
+++ WalletApi/entitys/Transaction.cs
@@ -0,0 +1,10 @@
+public class Transaction
+{
+    public int Id { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+    public int FromUserId { get; set; }
+    public int ToUserId { get; set; }
+    public User FromUser { get; set; }
+    public User ToUser { get; set; }
+}
